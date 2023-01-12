0d14f99145339552e5683521222805128d017eb7
homeworkdome <homeworkdome@outlook.com>
2023-01-12 17:28:22 +0300

    Добавьте файлы проекта.

---
+++ Задача 8-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+string path = @"D:\Учеба бим\Test.txt";
+Random random = new Random();
+using(StreamWriter sw = new StreamWriter(path, false))
+{
+    for (int i=0; i<10; i++)
+    {
+        sw.WriteLine(random.Next(-10, 10));
+    }
+}
+
+int Sum = 0;
+using (StreamReader sr = new StreamReader(path))
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Sum += (Convert.ToInt32(sr.ReadLine()));
+    }
+}
+Console.WriteLine("Сумма 10 случайных чисел в текстовом файле равна:");
+Console.WriteLine(Sum);
